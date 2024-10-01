40db7f5e285706f45039904dddc28662d1388e5d
Raul <raulgarcicespedes@gmail.com>
2024-10-01 19:44:35 +0200

    finished

--- main.ts
+++ main.ts
@@ -4,15 +4,13 @@ const numeros:number[] = [1, 6, 4, 99, 20, 13, 56, 7, 75, 47];
 function quicksort(array: number[]) : number[]{
     if (array.length <= 1) {return array;}
 
-    let pivote:number = array[Math.floor(array.length/2)];
-    console.log(pivote);
+    const pivote:number = array[Math.floor(array.length/2)];
 
-    let left = array.filter((elem:number) => elem < pivote);
-    let right = array.filter((elem:number) => elem > pivote);
+    const left = array.filter((elem:number) => elem < pivote);
+    const right = array.filter((elem:number) => elem > pivote);
 
     return quicksort(left).concat(pivote, quicksort(right));
 }
 
 const arrayOrdenado:number[] = quicksort(numeros);
-
 console.log(arrayOrdenado);
